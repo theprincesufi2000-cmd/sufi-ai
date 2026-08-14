a4f9ef80e5af9251dc420dce6660db0f30e4309c
theprincesufi2000-cmd <the.prince.sufi2000@gmail.com>
2026-08-14 07:14:27 +0300

    Add files via upload

---
+++ www/config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG={
+ GEMINI_API_KEY:"AQ.Ab8RN6LG8A7mtB56cRlwkgszaivMr1GFWmPxSo3uBeZGVqJRNw",
+ GEMINI_MODEL:"gemini-3.5-flash",
+ GEMINI_API_BASE:"https://generativelanguage.googleapis.com/v1beta",
+ FIREBASE:{apiKey:"AIzaSyCUV26Yu0GmGkA05gUVQMKqWsoH1Szm45M",authDomain:"gemini-ff6e0.firebaseapp.com",databaseURL:"https://gemini-ff6e0-default-rtdb.firebaseio.com",projectId:"gemini-ff6e0",storageBucket:"gemini-ff6e0.firebasestorage.app",appId:"1:33382194519:android:2481de1b2308bb6d9b7212"}
+};
